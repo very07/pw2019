dd1d83ccfc5b3cb03e8dc7358341a5e8a8123695
very07 <56446687+very07@users.noreply.github.com>
2019-11-13 19:47:05 +0100

    esercizio3

--- esercizio3/estrazioni_lotto.php
+++ esercizio3/estrazioni_lotto.php
@@ -32,3 +32,38 @@ foreach ($ruote as $key => $value){
     print_r ($value);
 }
 
+?>
+
+<div class="container">
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Città</th>
+      <th scope="colspan 4"></th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Bari</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+</body>
+</html>

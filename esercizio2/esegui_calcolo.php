4f440dc49928c32ef23e51e2df558617d6c445db
very07 <56446687+very07@users.noreply.github.com>
2019-11-13 18:46:07 +0100

    esercizio2

---
+++ esercizio2/esegui_calcolo.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+
+<?php
+
+print ($_GET['num1'] * $_GET['num2']);
+
+?>
+
+</br>
+<a href="/github/pw2019/esercizio2/lista_calcoli.php"> <-- Torna Indietro </a><br><br>
+
+
+
+
+
+
+
+
+</body>
+</html> 
---
+++ esercizio2/lista_calcoli.php
@@ -0,0 +1,37 @@
+<html>
+    <body>
+
+
+<?php
+
+for ($x=1; $x<=40; $x++){
+    $calcolo= $x * 3;
+?>
+
+<a href="/github/pw2019/esercizio2/esegui_calcolo.php?num1=<?=$calcolo?>&num2=<?=$calcolo?>"><?=$calcolo?>, <?=$calcolo?> </a></br>
+
+<?php
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
